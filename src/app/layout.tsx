a0d3d0cd3022f674e22f94dc2a81e77c3f3d4f9d
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-10-05 15:42:58 +0330

    TR-2 : tailwind config - set colors - set fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,32 @@
 import "./globals.css";
 
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
+
+const rooznameh = localFont({
+  src: [
+    {
+      path: "../../public/fonts/rooznameh/Rooznameh.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-rooznameh",
+});
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en" dir="rtl">
+    <html dir="rtl" className={`${figtree.variable} ${rooznameh.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          HEADER
+          دوره معماری ری اکت
         </header>
         <div className="flex-1 flex  justify-center items-center">
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import { Colors } from "./_components/colors/colors";
 
 export default function Home() {
   return (
-   <>
-     <div className='text-5xl '>React Application Architecture</div>
-   </>
-  )
+    <>
+      <Colors />
+    </>
+  );
 }
