a77c0a908c316a37559838eabe198f317d397d20
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-10-16 12:49:16 +0330

    TR-6 : implement Button component - storybook - debug Styles

---
+++ .storybook/preview.ts
@@ -0,0 +1,33 @@
+import '../src/app/globals.css';
+
+// Replace your-framework with the framework you are using (e.g., react, vue3)
+import { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    doc: {
+      controls: { exclude: ['controls'] },
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      showPanel: false,
+      // page: () => (
+      //   <>
+      //     <Title />
+      //     <Subtitle />
+      //     <Description />
+      //     <Controls  />
+      //     <Stories />
+      //   </>
+      // ),
+    },
+  },
+};
+
+export default preview;
